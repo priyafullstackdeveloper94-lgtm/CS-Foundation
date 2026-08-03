0fb0bd0e9678a66e07b3b09ca2a6edd6592de54b
priya-mern <priyafullstackdeveloper94@gmail.com>
2026-08-03 22:29:57 +0530

    Add javascript for dialog box showing

---
+++ js/home.js
@@ -0,0 +1,34 @@
+const classDialog = document.getElementById("classDialog");
+
+const startLearningBtn = document.getElementById("startLearningBtn");
+const exploreTopicsBtn = document.getElementById("exploreTopicsBtn");
+const okBtn = document.getElementById("okBtn");
+
+// Open Dialog
+startLearningBtn.addEventListener("click", function() {
+    classDialog.showModal();
+});
+
+exploreTopicsBtn.addEventListener("click", function() {
+    classDialog.showModal();
+});
+
+// Handle Selection
+okBtn.addEventListener("click", function() {
+
+    const selectedClass = document.querySelector(
+        'input[name="studentClass"]:checked'
+    );
+
+    if (!selectedClass) {
+        alert("Please select a class");
+        return;
+    }
+
+    if (selectedClass.value === "11") {
+        window.location.href = "chapter11.html";
+    } else if (selectedClass.value === "12") {
+        window.location.href = "chapter12.html";
+    }
+
+});
